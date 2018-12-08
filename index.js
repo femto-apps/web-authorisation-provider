8f3e2beb702b151def6ddc56ed1da38a3bf140fb
Alexander Craggs <popey@debenclipper.com>
2018-12-08 22:14:45 +0000

    Add in configuration system

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const Authorisation = require('./modules/Authorisation')
+const Config = require('../../modules/Config')
+
 const authorisation = new Authorisation()
+const config = new Config()
+
+const app = express()
+
+app.use(bodyParser.json())
 
 /* 
  * Register one or more statements for authentication.
@@ -28,14 +35,24 @@ app.post('/api/v1/statement', (req, res) => {
   // TODO: verify that the request came from a site
   // TODO: verify that the site can effect the specified resource
 
-  authorisation.registerStatements(req.body)
+  if (Array.isArray(req.body)) {
+    authorisation.registerStatements(req.body)
+  } else {
+    authorisation.registerStatement(req.body)
+  }
+
+  res.json({
+    message: 'added authorisation successfully'
+  })
 })
 
 /*
  * Checks whether a user can complete an action on the requested resource.
  */
-app.get('/api/v1/authorised', (req, res) => {
-  authorisation.check(req.body.resource, req.body.user, req.body.action)
+app.post('/api/v1/authorised', (req, res) => {
+  res.json({
+    authorised: authorisation.check(req.body.resource, req.body.user, req.body.action)
+  })
 })
 
-app.listen(config.port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(3000, () => console.log(`Example app listening on port ${3000}!`))
